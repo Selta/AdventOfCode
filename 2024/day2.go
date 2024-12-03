b9fa41e2012b3f5b771f49012328b522e5ed9315
selta <RJSalamon@hotmail.com>
2024-12-02 19:25:14 -0800

    day 2 part 2 with a small pile of learning

--- 2024/day2.go
+++ 2024/day2.go
@@ -18,13 +18,21 @@ func main() {
         fmt.Println("Error reading reports:", err)
 	return
     }
-    var safeReports []Report
+    var safeReports, badReports, dampenedReports []Report
     for _, report := range reports {
-        if inspectReport(report) {
+	if inspectReport(report) {
 	    safeReports = append(safeReports, report)
+	} else {
+	    badReports = append(badReports, report)
 	}
     }
-	fmt.Println("Safe Reports:", len(safeReports))
+    fmt.Println("Safe Reports: ", len(safeReports))
+    for _, report := range badReports {
+	if dampenerCheck(report) {
+	    dampenedReports = append(dampenedReports, report)
+	}
+    }
+    fmt.Println("Safe and \"Dampened\" Reports: ", len(dampenedReports)+len(safeReports))
 }
 
 func parseReports(filename string) ([]Report, error) {
@@ -51,11 +59,10 @@ func parseReports(filename string) ([]Report, error) {
     return reportList, nil
 }
 
-func inspectReport(report Report) bool {
+func inspectReport(report Report) (bool) {
     // levels must be all increasing or all decreasing
     levelsIncreasing := true
     levelsDecreasing := true
-
     for i := 1; i < len(report); i++ {
        	if report[i] > report[i-1] {
 	    levelsDecreasing = false
@@ -72,5 +79,18 @@ func inspectReport(report Report) bool {
 	    return false
 	} 
     }
+    if (levelsIncreasing && levelsDecreasing) {
+    }
     return levelsIncreasing || levelsDecreasing
+}
+
+func dampenerCheck (report Report) (bool) {
+    for i := 0; i < len(report); i++ {
+	modifiedReport := append([]int(nil), report...)
+	modifiedReport = append(modifiedReport[:i], modifiedReport[i+1:]...)
+	if inspectReport(modifiedReport) {
+	    return true
+	}
+    }
+    return false
 }
